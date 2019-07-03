9a61190cedcb6cfb07cea9e26b3e4b8812b54b90
WilliamVDavis <50843184+WilliamVDavis@users.noreply.github.com>
2019-07-03 16:56:53 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
     
-import React, {Component} from '../node_modules/react';
+import React, {Component} from 'react';
 import './App.css';
-import {BrowserRouter as Router, Link, Redirect} from '../node_modules/react-router-dom';
-import Route from '../node_modules/react-router-dom/Route';
+import {BrowserRouter as Router, Link, Redirect} from 'react-router-dom';
+import Route from 'react-router-dom/Route';
 
 const User = (params)=> {
   return (<h1>Welcome User {params.username}</h1>)
@@ -18,17 +18,16 @@ render() {
   return (
     <Router>
     <div className="App">
+    
       <ul>
         <li>
-
-
-
-<Route path="/" exact strict render={
-  ()=> {
+<Route path="/" exact strict render={ 
+  ()=> { 
     return ( <h1>Welcome Home Chirper</h1>);
   }
   }/>
-  <Route path="/about" exact strict render={
+  
+  <Route path="/feed" exact strict render={
   ()=> {
     return ( <h1>About Time You Signed Up</h1>);
   }
@@ -39,9 +38,21 @@ render() {
 <Link to="/">Home</Link>
 </li>
 <li>
-<Link to="/about">About</Link>
+<Link to="/feed">Feed</Link>
 </li>
 </ul>
+<form>
+  <div class="form group">
+    <label for="InputUsername">
+  Username</label>
+  <input type="text" name="username" placeholder="Enter email"></input>
+  </div>
+  <div class="form group">
+  <label for="InputPassword">
+    Password </label>
+  <input type="text" name="password" placeholder="Enter password"></input>
+  </div>
+</form>
 <input type="button" value="Log in" onClick={this.loginHandle.bind(this)}/>
     </div>
     </Router>
