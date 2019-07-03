22477c8846ec0f378223387de1f528730b3f77bb
WilliamVDavis <50843184+WilliamVDavis@users.noreply.github.com>
2019-07-03 16:59:28 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
+
+const items = require('../react-express/routes/api/items');
+
+
+
+// Bodyparser Middleware
+app.use(bodyParser.json())
+
+// DB Config
+const db = require('../react-express/config/keys').mongoURI;
+
+//Connect to Mongo
+mongoose
+.connect(db, { useNewUrlParser: true })
+.then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err));
+
+
+
+//Use Routes
+app.use('/api/items', items);
+
 const PORT = process.env.PORT || 5000 
 
-app.use('/', express.static(path.join(__dirname, 'public')));
-app.use('/', express.static(path.join(__dirname, 'src')));
-
-app.get('/', function(req, res){
-     res.sendFile(path.join(__dirname, 'public/index.html'));
-});
 
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`)); 
